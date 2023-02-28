d599972928977eb77ab8e7852829c88c64c82e25
Dymedrol <dmitrykob@gmail.com>
2023-02-28 20:40:42 +0700

    daesktop zoom added

--- source/js/pdp.js
+++ source/js/pdp.js
@@ -5,6 +5,11 @@ export const initPdp = () => {
         return
     }
 
+    const gallery = $('.laoli-pdp-gallery');
+    const galleryWrapper = gallery.find('.laoli-pdp-gallery-wrapper');
+    const goBack = $('.laoli-pdp-desktop-goback')
+    const back = $('.laoli-pdp-desktop-back')
+
     const detailsButton = $('.js-details-button');
     const detailsWrapper = $('.js-details-wrapper')
     const careButton = $('.js-care-button')
@@ -34,6 +39,55 @@ export const initPdp = () => {
         transitionInterval: .3,
         progressBarClassName: 'laoli-pdp-mobile-carousel-control',
         scrollerClassName: 'laoli-pdp-mobile-carousel-control-indicator'
+    });
+
+    const showGallery = function() {
+        goBack.hide();
+        back.show();
+        gallery.fadeIn(200);
+        gallery.focus();
+    }
+
+    const hideGallery = function() {
+        galleryWrapper.animate({
+          scrollTop: 0
+        }, 0);
+        goBack.show();
+        back.hide();
+        gallery.fadeOut(200);
+    }
+
+    $('.laoli-pdp-desktop-pic').click(function() {
+        showGallery();
+
+        const index = $(this).attr('data-index');
+
+        const attr = "img[data-index*=" + index + "]";
+        const targetPic = gallery.find(attr);
+        let offset = targetPic.position().top;
+
+        if (index == 1) {
+            offset = 0;
+        }
+
+        console.log(offset)
+
+        galleryWrapper.animate({
+          scrollTop: offset
+        }, 0);
+
+        gallery.on('blur',function() {
+            hideGallery();
+        });
+
+
+        back.click(function() {
+            hideGallery();
+        });
+
+
+
+
     });
 
 }
