9dc550529cd28930a2aa140b3387b47b06183990
Dymedrol <dmitrykob@gmail.com>
2023-03-01 19:11:34 +0700

    lazy load + srcset

--- source/js/registration.js
+++ source/js/registration.js
@@ -22,43 +22,32 @@ export const initRegistration = () => {
     form.submit(function(e) {
         e.preventDefault();
 
-        const name = form.find('.laoli-registration-input-name');
-        const email = form.find('.laoli-registration-input-email');
+        const name = form.find('.laoli-registration-input-name').val();
+        const email = form.find('.laoli-registration-input-email').val();
 
-        const options = {
-          method: 'POST',
-          headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
-          body: JSON.stringify({profiles: [{email: email, name: name}]})
-        };
+        var settings = {
+          "async": true,
+          "crossDomain": true,
+          "url": "https://manage.kmail-lists.com/ajax/subscriptions/subscribe",
+          "method": "POST",
+          "headers": {
+            "content-type": "application/x-www-form-urlencoded",
+            "cache-control": "no-cache"
+          },
+          "data": {
+            "g": list_id,
+            "$fields": "$first_name",
+            "email": email,
+            "$first_name": name,
+          }
+        }
 
-        fetch('https://a.klaviyo.com/api/v2/list/SdcCiU/subscribe?api_key=ShtSDp', options)
-          .then(response => response.json())
-          .then(response => console.log(response))
-          .catch(err => console.error(err));
-
-        // var settings = {
-        //   "async": true,
-        //   "crossDomain": true,
-        //   "url": "https://manage.kmail-lists.com/ajax/subscriptions/subscribe",
-        //   "method": "POST",
-        //   "headers": {
-        //     "content-type": "application/x-www-form-urlencoded",
-        //     "cache-control": "no-cache"
-        //   },
-        //   "data": {
-        //     "g": list_id,
-        //     "$fields": "$first_name",
-        //     "email": "ddd@ddd.com",
-        //     "$first_name": "name name",
-        //   }
-        // }
-
-        // $.ajax(settings).done(function (response) {
-        //   if (response.success) {
-        //     form.hide();
-        //     registration.find('.laoli-registration-form-success').show();
-        //   }
-        // });
+        $.ajax(settings).done(function (response) {
+          if (response.success) {
+            form.hide();
+            registration.find('.laoli-registration-form-success').show();
+          }
+        });
     });
 
 
